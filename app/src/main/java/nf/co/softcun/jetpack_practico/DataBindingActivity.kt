eaecfbd1724718b8129e25853addb5e20040d842
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-02-27 02:21:40 -0600

    modulo de databinding
    concluyendo con lo entendido :(

---
+++ app/src/main/java/nf/co/softcun/jetpack_practico/DataBindingActivity.kt
@@ -0,0 +1,42 @@
+package nf.co.softcun.jetpack_practico
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import nf.co.softcun.jetpack_practico.databinding.ActivityDataBindingBinding
+import nf.co.softcun.jetpack_practico.modelo.Usuario
+
+class DataBindingActivity : AppCompatActivity() {
+
+    private lateinit var mListaNombres: ArrayList<Usuario>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_data_binding)
+
+        var binding = DataBindingUtil.setContentView<ActivityDataBindingBinding>(this, R.layout.activity_data_binding)
+
+        val usuarioCodigo = Usuario("Jesus", "Ferrer")
+
+        binding.usuario = usuarioCodigo
+
+        mListaNombres = ArrayList()
+        for (count in 1..5){
+            val usuario = Usuario("Jesus $count", "Ferrer $count")
+            mListaNombres.add(usuario)
+        }
+
+        binding.listaUsuario = mListaNombres
+
+        val textoGrande = false
+        binding.textoGrande = textoGrande
+
+
+        /*setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }*/
+    }
+
+}
---
+++ app/src/main/java/nf/co/softcun/jetpack_practico/modelo/Usuario.kt
@@ -0,0 +1,3 @@
+package nf.co.softcun.jetpack_practico.modelo
+
+data class Usuario(val nombre:String, val apellido:String)
